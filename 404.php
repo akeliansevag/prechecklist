1e3ec3538b82723199e5be59f8881b56f73c5b22
montyholdingdev <155431830+montyholdingdev@users.noreply.github.com>
2024-04-15 15:23:37 +0300

    .

--- 404.php
+++ 404.php
@@ -3,7 +3,6 @@
   <div class="section-content text-center error-page">
     <h2>404</h2>
     <h3>Page Not Found</h3>
-    <p>We're Sorry, the page you requested could not be found.<br /> Please go back to the <a href="<?=home_url();?>">homepage</a></p>
   </div>
 </section>
 <?php get_footer(); ?>
